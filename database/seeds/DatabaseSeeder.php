feeffbf33fc84aa8f35345b3f9801f07e9b39feb
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-05-29 14:16:43 +0700

    Make wallet model, migration and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        // $this->call(RolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         $this->call(UserTableSeeder::class);
+        $this->call(WalletTableSeeder::class);
     }
 }
---
+++ database/seeds/WalletTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Wallet;
+
+class WalletTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Wallet::create([
+            "balance" => 1000000,
+            "user_id" => 1
+        ]);
+        Wallet::create([
+            "balance" => 1000000,
+            "user_id" => 2
+        ]);
+        Wallet::create([
+            "balance" => 1000000,
+            "user_id" => 3
+        ]);
+    }
+}
