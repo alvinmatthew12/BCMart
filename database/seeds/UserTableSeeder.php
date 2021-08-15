ded75e84c092a9b580f68e37b0e26e21a8335e02
cindyadonia <cindy.adonia3@gmail.com>
2021-08-15 20:49:51 +0700

    Add new user on Seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -30,5 +30,11 @@ class UserTableSeeder extends Seeder
             "password" => bcrypt("alvin"),
             "role_id" => 3
         ]);
+        User::create([
+            "name" => "Cindy Adonia",
+            "email" => "cindy@mail.com",
+            "password" => bcrypt("alvin"),
+            "role_id" => 3
+        ]);
     }
 }
